bfd173539e8f2e2c8fcd072e7bc32d9000f94095
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2025-06-13 23:07:51 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,10 @@ import { Link, useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
-
   return (
     <div>
-      <Button color="inherit" onClick={handleLogout}>
-        Logout
-      </Button>
-      <h1>CRM Home Page</h1>;
+     
+      <h1>CRM Home Page</h1>
       <Link to="/customers">Go to Customer List Page</Link>
     </div>
   );
